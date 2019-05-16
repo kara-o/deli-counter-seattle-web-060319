1d239a873ad3a48d1116dc7b3c390d87a7e75250
Kara O <karajo.odle@gmail.com>
2019-05-16 23:26:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,5 @@ katz_deli = []
 def line(array)
   if array.length == 0 
     puts "The line is currently empty."
-  elsif array.length > 0 
-    for i in array[0..array.length]
-      puts "The line is currently: "
+
     
