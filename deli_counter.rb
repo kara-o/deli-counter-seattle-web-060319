46a453e8b68ecffd2e82ef686e88810139d23610
Kara O <karajo.odle@gmail.com>
2019-05-16 23:35:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,12 @@ katz_deli = []
 def line(array)
   if array.length == 0 
     puts "The line is currently empty."
+  end
+  
+  new_array = []
+  
+  for i in (0..array.length)
+    new_array << array[i]
+    
 
     
