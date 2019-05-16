9246fd15842f7ca3eea80c35ed4090a5b503146e
Kara O <karajo.odle@gmail.com>
2019-05-16 23:24:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  elsif array.length > 0 
+    for i in array[0..array.length]
+      puts "The line is currently: "
+    
