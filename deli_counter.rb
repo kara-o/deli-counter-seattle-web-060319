2c309c345ac3b24845c34e61a9f23bee543bd454
Kara O <karajo.odle@gmail.com>
2019-05-17 00:22:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -24,4 +24,29 @@ def take_a_number(array, name)
   puts "Welcome, #{name}. You are number #{array.length} in line."
 end
 
-def now_serving
+def now_serving(array)
+  
+  if array.length
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
